9ac41b404d7c26f56b764a70ab941367b87685f4
Aijamal Jusupova <109210016+Aijus2022@users.noreply.github.com>
2023-12-31 12:22:08 +0000

    Update Carousel.js
    Added JS code to the file

--- Carousel.js
+++ Carousel.js
@@ -1,1 +1,30 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+  const carousel = document.querySelector(".carousel");
+  const slides = document.querySelectorAll(".slide");
+
+  let index = 0;
+
+  function updateCarousel() {
+    carousel.style.transform = `translateX(${-index * 100}%)`;
+  }
+
+  function showNextSlide() {
+    index = (index + 1) % slides.length;
+    updateCarousel();
+  }
+
+  function showPrevSlide() {
+    index = (index - 1 + slides.length) % slides.length;
+    updateCarousel();
+  }
+
+  // Adjust event listeners as needed (e.g., arrow keys, buttons)
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      showPrevSlide();
+    } else if (event.key === "ArrowRight") {
+      showNextSlide();
+    }
+  });
+});
